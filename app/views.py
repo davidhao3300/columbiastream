6a7b3f0f0d05bfcb82520f98d34e21d6bbffe2fe
David H <davidhao3300@yahoo.com>
2014-04-08 01:49:43 -0400

    Using Flask-Tweepy instead of tweepy

--- app/views.py
+++ app/views.py
@@ -4,16 +4,14 @@
 from flask import redirect, session, url_for, request
 from flask.ext.sqlalchemy import get_debug_queries
 from app import app, db, lm
-import tweepy
+from flask.ext.tweepy import Tweepy
 
 @app.route('/', methods = ['GET'])
 def index():
-    if 'oauth_token' in request.form:
-        print request.form['oauth_token']
-    auth = tweepy.OAuthHandler("HJC58RkbqZeHo0Z6sesPrKWRc", "2pDKqITFdoqRZlVFJvQ1iDfE6kyMqlDxcDlOpC6NuV6nAn5Jl4")
-    try:
-        redirect_url = auth.get_authorization_url()
-    except tweepy.TweepError:
-        print 'Error! Failed to get request token.'
-    session['request_token'] = (auth.request_token.key, auth.request_token.secret)
-    return redirect(redirect_url)
+	app.config.setdefault('TWEEPY_CONSUMER_KEY', 'HJC58RkbqZeHo0Z6sesPrKWRc')
+	app.config.setdefault('TWEEPY_CONSUMER_SECRET', '2pDKqITFdoqRZlVFJvQ1iDfE6kyMqlDxcDlOpC6NuV6nAn5Jl4')
+	app.config.setdefault('TWEEPY_ACCESS_TOKEN_KEY', '1007063444-nU2zSoqqvoOrKtttK6RBL4YK7PQ0UvnZAVb0GFo')
+	app.config.setdefault('TWEEPY_ACCESS_TOKEN_SECRET', 'C7SYtnNCX5mAZLFvhP5QaYam5iTnvokOfKQ7LlDFEBEL4')
+
+	tweepy = Tweepy(app)
+	return "Hi!"
