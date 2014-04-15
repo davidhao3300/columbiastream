a450314e79bcd3e849d35787a9b3fc7022d26e22
David H <davidhao3300@yahoo.com>
2014-04-15 00:36:09 -0400

    colors

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,5 +16,34 @@ $(function() {
 
 function prepend(tweet)
 {
-    $('#test').prepend("<span></span>").prepend("<p>Sentiment: "+tweet.sent+"</p>").prepend("<p>Time: "+tweet.time+"</p>").prepend("<p>Text: "+tweet['text']+"</p>");
+    var color = "#000000";
+    if (tweet.sent < 0)
+    {
+        if(tweet.set > -0.75) color = "#" + (-1*parseInt(tweet.sent*340)).toString(16) + "0000";
+        else color = "#FF0000";
+    }
+    else if (tweet.sent > 0)
+    {
+        if (tweet.sent < 0.75)
+        {
+            color = "#00" + (parseInt(tweet.sent *340)).toString(16) +"00";
+        }
+        else color = "#00FF00";
+    }
+    /*
+    if (tweet.sent >= 0.3)
+    {
+        color = "#00ff00";
+    }
+    else if (tweet.sent > -0.3)
+    {
+        var red = (127 - 425 * tweet.sent);
+        var green = (127 + 425 * tweet.sent);
+        var blue = (425 * tweet.sent);
+        if (blue < 0) blue += 128;
+        else blue = 128- blue;
+        color = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+    }*/
+    $('#test').prepend("<span></span>").prepend("<p>Sentiment: "+tweet.sent+"</p>").prepend("<p>Time: "+tweet.time+"</p>").prepend("<p style=\"color:"+color+"\">Text: "+tweet['text']+"</p>");
+
 }
