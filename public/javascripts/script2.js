ce79908028c6fabc25dd6fec53190d4750d89de5
David H <davidhao3300@yahoo.com>
2014-04-17 03:19:57 -0400

    typo

--- public/javascripts/script2.js
+++ public/javascripts/script2.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
         setInterval(function()
         {
             time.setTime(time.getTime()-1000*MULT/2);
-            v//ar actualtime = new Date(time.getTime()-4*60*60*1000);
+            //var actualtime = new Date(time.getTime()-4*60*60*1000);
             $("#current-time").text(time.toLocaleString());
             var curr = parseInt(time.getTime()/1000);
             console.log(curr+" "+parseInt(tweets[num].time));
