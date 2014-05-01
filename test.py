321bfc86cda4fbf7c1f123929f8d19f53cb0388d
David H <davidhao3300@yahoo.com>
2014-04-30 20:30:29 -0400

    new tweets

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
-import json;
+import json
 from datetime import datetime
-from tweepy import OAuthHandler
-from tweepy import Stream, Status
-consumer_key="HJC58RkbqZeHo0Z6sesPrKWRc"
-consumer_secret="2pDKqITFdoqRZlVFJvQ1iDfE6kyMqlDxcDlOpC6NuV6nAn5Jl4"
-access_token="1007063444-nU2zSoqqvoOrKtttK6RBL4YK7PQ0UvnZAVb0GFo"
-access_token_secret="C7SYtnNCX5mAZLFvhP5QaYam5iTnvokOfKQ7LlDFEBEL4"
 
 test = open("tweets.txt");
 data = json.loads(test.read());
-for i in range(1, len(data)):
-	if data[i]['time'] < data[i-1]['time']:
-		print data[i]['time']
-	print datetime.fromtimestamp(data[i]['time'])
+test.close()
+i = 0
+for tweet in data:
+	print datetime.fromtimestamp(tweet['time'])
+test = open("tweets.txt",'w')
+test.write(json.dumps(data));
+test.close();
